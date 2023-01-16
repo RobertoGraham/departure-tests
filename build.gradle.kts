b3ca0bcca6ec168e2f05d7e65aa332f46bc0c081
Roberto Graham <robertogrhm@gmail.com>
2023-01-16 17:34:27 +0000

    chore: Playwright compatible image

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     java
     groovy
+    application
+}
+
+application {
+    mainClass.set("com.microsoft.playwright.CLI")
 }
 
 group = "io.github.robertograham"
@@ -13,6 +18,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.microsoft.playwright:playwright:1.28.1")
     testImplementation("org.spockframework:spock-core:2.3-groovy-4.0")
     testImplementation("org.testcontainers:testcontainers:1.17.6")
     testImplementation("org.slf4j:slf4j-simple:2.0.5")
@@ -20,7 +26,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("org.apache.commons:commons-lang3:3.12.0")
-    testImplementation("com.microsoft.playwright:playwright:1.28.1")
 }
 
 tasks {
